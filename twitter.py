b1a55a4ee78d084d464ef6de7b690200afbc4d7c
Kevin <kevinatgis@gmail.com>
2016-06-27 19:35:22 -0400

    fixed argv

--- twitter.py
+++ twitter.py
@@ -1,14 +1,15 @@
 # twitter playground
 # kevinatgis
+# tweepy: https://github.com/tweepy/tweepy
 
 import tweepy
 import sys
 
 # static
-consumer_key = sys.argv[0]
-consumer_secret = sys.argv[1]
-access_token = sys.argv[2]
-access_token_secret = sys.argv[3]
+consumer_key = sys.argv[1]
+consumer_secret = sys.argv[2]
+access_token = sys.argv[3]
+access_token_secret = sys.argv[4]
 
 # oauth info
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
