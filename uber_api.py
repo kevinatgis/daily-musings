9bb65291cc7dd4a18e3b9acaae78c9b5ca27b50e
kevin <kevinatgis@gmail.com>
2016-06-29 12:00:41 -0400

    [UBER]: finished time-estimates functionality

--- uber_api.py
+++ uber_api.py
@@ -1,31 +1,41 @@
-# using the uber API
-# no need to request a ride, only to gather general information
-# oauth is only used to avoid making the user give their username/pw to a third party
-# kevinatgis
-
+# using the Uber API V1 w python
+# todo:
+# add exception handling
 # combine uber estimates timing with google maps navigation
+# kevinatgis 2016
 
 import requests
+import json
+from pprint import pprint
 
 # keys
-server_token = ''
-headers = {'Authorization': 'Token {}'.format(server_token)}
-url = 'https://api.uber.com/v1/'
+server_token 	= '' # https://developer.uber.com/docs/rides/getting-started
+headers 		= {'Authorization': 'Token {}'.format(server_token)}
+url 			= 'https://api.uber.com/v1/'
 
-# time estimate to get all available Uber Products
-def time_estimates(lat,lon,p_id=''):
-	# curl -H 'Authorization: Token INSERT_TOKEN_HERE' \
-	# 'https://api.uber.com/v1/estimates/time?start_latitude=37.43.645143&start_longitude=-79.380655'
-
+# time estimate to get all available Uber Products, format: json output or print (debug)
+def time_estimates(lat,lon,p_id='',format='json'):
+	# https://developer.uber.com/docs/rides/api/v1-estimates-time
 	if (p_id==''):
 		products = requests.get(url+'estimates/time?start_latitude={}&start_longitude={}'.format(lat,lon), headers=headers)
 	else:
 		products = requests.get(url+'estimates/time?start_latitude={}&start_longitude={}&product_id={}'.format(lat,lon,p_id), headers=headers)
 
-	print(products.text)
+	# turns the request obj to a dict
+	products_json = products.json()
+
+	# if none available, json defaults: {'times': []} 
+	if format=='json':
+		if len(products_json['times']) == 0:
+			return None
+		else:
+			return products_json['times']
+	elif format=='print':
+		# pretty print the json
+		pprint(products_json, indent=4)
 
 # products @ Union Station, TO
-time_estimates(43.645143,-79.380655)
+time_estimates(43.645143,-79.380655,format='print')
 
 
 
