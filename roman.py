55b5aeddf91d90cb29537f8e6e61a5f40e87e84b
Kevin <kevinatgis@gmail.com>
2017-11-12 18:46:47 -0500

    added decode
    also, turns out that python2 defaults to integer divison for x//y or x/y. So, no need to make it python3 exclusive.

--- roman.py
+++ roman.py
@@ -1,14 +1,13 @@
 '''Decodes and encodes integers from 1-4999 in Roman numerals'''
 
-import sys
+# import sys
+# if sys.version_info[0] < 3:
+#	print('This requires Python 3.')
+#	quit()
 
-if sys.version_info[0] < 3:
-	print('This requires Python 3.')
-	quit()
 
 roman_numerals = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
 roman_numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-roman_dict = dict(zip(roman_numbers,roman_numerals))
 
 
 def roman_numeral_encode(number):
@@ -20,7 +19,6 @@ def roman_numeral_encode(number):
 	Returns: str
 
 	'''
-	
 	try:
 		if number > 4999 or number < 1:
 			raise ValueError('Accepts only values from 1-4999.')
@@ -28,11 +26,12 @@ def roman_numeral_encode(number):
 			return 'nulla' #romans didn't have a 0 numeral, rather the word nulla/null
 
 		output = ''
+		roman_encode = dict(zip(roman_numbers,roman_numerals))
 
 		for x in roman_numbers:
 			if number >= x:
 				multiple = number//x #int division (no remainder)
-				output+=roman_dict[x]*multiple #outputs numeral X-times
+				output+=roman_encode[x]*multiple #outputs numeral X-times
 				number%=x #modulo->remainder
 
 		return(output)
@@ -40,4 +39,28 @@ def roman_numeral_encode(number):
 	except TypeError as e:
 		print('Accepts only integers.')
 
+
+def roman_numeral_decode(numeral):
+	'''Decodes numeral to int
+
+	Keyword arguments:
+	numeral -- str
+
+	Returns: int
+
+	'''
+	try:
+		roman_decode = dict(zip(roman_numerals,roman_numbers))
+
+		output = 0 #returned output
+		start_pos = 0
+
+		for letters in roman_numerals:	
+			while(numeral[start_pos:start_pos+len(letters)]) == letters: # iterates through letters in numeral based on length
+				output += roman_decode[letters] # adds to output
+				start_pos += len(letters) # changes next starting position after iterating
+
+		return(output)
+	except TypeError:
+		print('Accepts only strings.')
 
